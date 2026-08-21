3a4c87ec7b2124441af9ec81c2c61303555ebc84
Juancho024 <jerp0001@ce.pucmm.edu.do>
2026-08-20 23:53:37 -0800

    Version: 1.5.27 Cambios: - arregle una variable en Preferencia - etc

--- src/main/java/com/jrdev/whatsappplatform/model/Preferencia.java
+++ src/main/java/com/jrdev/whatsappplatform/model/Preferencia.java
@@ -1,8 +1,13 @@
 package com.jrdev.whatsappplatform.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Table(name = "preferencias")
